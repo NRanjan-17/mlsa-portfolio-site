51a91642319e04fca1164dc1b684cd6b0f22555e
NRanjan-17 <nalinishranjan05@gmail.com>
2024-02-13 03:49:42 +0000

    Update information related to me

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nalinish Ranjan",
+  title: "Developer & Microsoft Learn Student Ambassador",
+  email: "nalinishranjan05@gmail.com",
+  gitHub: "NRanjan-17",
+  instagram: "hyperpower_17",
+  linkedIn: "nranjan17",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "ranjan_nalinish",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
